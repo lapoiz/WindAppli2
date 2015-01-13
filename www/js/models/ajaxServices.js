6afcf3dc4014a42ca58fc8467ceaa58886fffdce
lapoiz <poizatd@hotmail.com>
2015-01-14 00:42:06 +0100

    New appli

--- www/js/models/ajaxServices.js
+++ www/js/models/ajaxServices.js
@@ -9,7 +9,6 @@ laPoizWindApp.factory('AjaxServices',  function ($http, $q) {
             $http.get('http://www.lapoiz.com/Wind/web/app.php/json/spot/list')
                 //si nous arrivons a récupérer les spots alors :
                 .success(function(data, status){
- //                   console.log("json returned: " + JSON.stringify(data));
                     factory.spots = data; // notre objets posts sera égal au fichier json(data)
                     // et on réenvois les spots
                     deferred.resolve(factory.spots);
@@ -17,24 +16,19 @@ laPoizWindApp.factory('AjaxServices',  function ($http, $q) {
                 // si nous y arrivons pas :
                 .error(function(data, status){
                     //on réenvois un message d'erreur
-                    console.log("AjaxServices - Erreur returned: " + status );
+                    console.warn("AjaxServices - Erreur returned: " + status );
                     deferred.reject('Impossible de récupérer les spots, ERROR 999 - coté serveur');
                 });
             return deferred.promise; // return notre promesses
         },
         dataSpot : false,
         getDataSpot : function(idSpot){
-            //alert("envoie requette Ajax");
-            //console.log("AjaxServices -> in getDataSpot ");
-            //console.log("URL: "+'http://localhost/Wind/web/app_dev.php/json/spot/'+idSpot);
             var deferred = $q.defer();
             try {
             //$http.get('http://localhost/Wind/web/app_dev.php/json/spot/'+idSpot)
             $http.get('http://www.lapoiz.com/Wind/web/app.php/json/spot/'+idSpot)
             //$http.get('test/json/test.json')
                 .success(function(data, status){
-                    alert("reception requette Ajax");
-                    //console.log("json returned: " + JSON.stringify(data));
                     factory.dataSpot = data;
                     deferred.resolve(factory.dataSpot);
                 })
